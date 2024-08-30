400567725a85b0619c889f3c2b0bdf412268c89b
Abhxaami <122599340+Abhxaami@users.noreply.github.com>
2024-08-30 03:21:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const productRoute = require("./routes/product_route.js");
+const cartRoute = require("./routes/cart_route.js");
+const userRoute = require("./routes/user_route.js");
+const app = express();
+
+
+app.use(cors());    
+
+app.use(morgan('dev'))
+
+app.use("/api", cartRoute);
+app.use("/adminPanel", productRoute);
+app.use("/accounts", userRoute);
+
+const PORT = process.env.PORT || 9000;
+
+const db ="  ";
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("DB Connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello...");
+});
+
+app.listen(PORT, () => console.log(`Listening On Port:${PORT}`));
